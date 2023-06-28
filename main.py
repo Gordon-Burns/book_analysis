3056be69e97709978f35fe76a3370ddae96dd255
Gordon Burns <gordonburns@live.co.uk>
2023-06-28 14:23:52 +0100

    Returned all sentences with word love

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ with open("miracle_in_the_andes.txt", "r", encoding="utf8") as file:
 # Print how many chapters in the book
 print(x)
 print(len(x))
+
+# Which sentences contain the word love
+
+pattern = re.compile("[A-Z]{1}[^.]*[^a-zA-Z]+love[^a-zA-Z]+[^.]*.")
+findings = re.findall(pattern,content)
+for index,finding in enumerate(findings):
+    Num = str(index)
+    print(f"{Num} {finding}")
